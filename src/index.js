77e24213083eb20475ab7c833878f9c25b00046e
Majid Koohjani <majidkoohjani03@gmail.com>
2023-04-19 02:41:42 +0330

    افزودن ارتباط با ما و درباره ما و روت های مربوطه

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
 import Error404 from "./pages/Errors/Error404";
 import "bootstrap/dist/css/bootstrap-reboot.min.css";
 import "bootstrap/dist/css/bootstrap-grid.min.css";
@@ -18,7 +20,13 @@ root.render(
           ["/login", "/signin", "/ورود"].map((route, index) => <Route path={route} key={index} element={<Login />} />)
         }
         {
-          ["/", "/home", "/dashboard", "/خانه"].map((route, index) => <Route path={route} key={index} element={<Dashboard />} />)
+          ["/", "/home", "/dashboard", "/services", "/خانه"].map((route, index) => <Route path={route} key={index} element={<Dashboard />} />)
+        }
+        {
+          ["/about", "/about-us", "/درباره", "/درباره-ما"].map((route, index) => <Route path={route} key={index} element={<About />} />)
+        }
+        {
+          ["/contact", "/contact-us", "/تماس", "/تماس-با-ما"].map((route, index) => <Route path={route} key={index} element={<Contact />} />)
         }
         <Route path="*" element={<Error404 />}/>
       </Routes>
