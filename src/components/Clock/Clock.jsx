228d43598965a26208da81a4ccf92f4f18ff1d40
Majid Koohjani <majidkoohjani03@gmail.com>
2023-12-10 09:34:31 +0330

    Fix datetime

--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -4,22 +4,23 @@ import Storage from "../../core/helpers/Storage";
 export default function Clock() {
     const [time, setTime] = useState();
     const language = Storage.getLanguage();
-
+    
     useEffect(() => {
         setInterval(() => {
             let dt = new Date();
 
             let finalString = "";
+            const day = dt.getDate()
 
             if (language === "fa") {
-                finalString = `${dt.getHours()}:${dt.getMinutes()}:${dt.getSeconds()} ${dt.getFullYear()}/${dt.getMonth()}/${dt.getDay()}`;
+                finalString = `${dt.getHours()}:${dt.getMinutes()}:${dt.getSeconds()} ${dt.getFullYear()}/${dt.getMonth() + 1}/${day}`;
             } else {
-                finalString = `${dt.getFullYear()}/${dt.getMonth()}/${dt.getDay()} ${dt.getHours()}:${dt.getMinutes()}:${dt.getSeconds()}`;
+                finalString = `${dt.getFullYear()}/${dt.getMonth() + 1}/${day} ${dt.getHours()}:${dt.getMinutes()}:${dt.getSeconds()}`;
             }
 
             setTime(finalString);
         }, 1000);
     }, []);
 
-    return time;
+    return <span>{time}</span>;
 }
